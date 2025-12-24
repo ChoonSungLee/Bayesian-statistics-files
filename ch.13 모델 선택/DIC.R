cc12af828b4e7e7e94df84569bd9d2bdb81ce367
ChoonSungLee <ddormapuko503@gmail.com>
2025-12-24 10:53:07 +0900

    code change

--- ch.13 모델 선택/DIC.R
+++ ch.13 모델 선택/DIC.R
@@ -21,18 +21,27 @@ fit <- stan_glm(data ~ 1,
 posterior_samples <- as.matrix(fit)  
 log_lik <- log_lik(fit)              
 
-# D_hat calculation (deviance from parameter mean)
-mean_mu <- mean(posterior_samples[, "(Intercept)"])  
-D_hat <- -2 * sum(dnorm(data, mean = mean_mu, sd = 1, log = TRUE))
+# D_hat calculation (deviance at parameter mean)
+# Note: Since the model estimates sigma, mean_sigma must be used.
+mean_mu <- mean(posterior_samples[,"(Intercept)"])  
+mean_sigma <- mean(posterior_samples[,"sigma"])
 
-# D_bar calculation (deviance mean from posterior sampling)
-D_bar <- mean(-2 * log_lik)
+D_hat <- -2 * sum(dnorm(data, mean = mean_mu, 
+                        sd = mean_sigma, log = TRUE))
 
-# p_D, DIC calculation
+# D_bar calculation (mean of deviances)
+# Sum log-likelihoods across all data points for each MCMC sample first.
+sum_log_lik_per_iter <- rowSums(log_lik) 
+D_bar <- mean(-2 * sum_log_lik_per_iter)
+
+# p_D, DIC calculate
 p_D <- D_bar - D_hat
 DIC <- D_bar + p_D
+
 cat(sprintf("D_hat: %.4f\n", D_hat))
-cat(sprintf("DIC: %.4f\n", DIC))
+cat(sprintf("D_bar: %.4f\n", D_bar))
+cat(sprintf("p_D:   %.4f\n", p_D))
+cat(sprintf("DIC:   %.4f\n", DIC))
 
 
 
