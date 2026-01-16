010e4d4b7745b5b5f9f06d5e35c871e917d2c378
labe74 <ddormapuko503@gmail.com>
2026-01-17 07:54:20 +0900

    file change

--- ch.16 카이제곱 분포/chisq_independ_Bayes.R
+++ ch.16 카이제곱 분포/chisq_independ_Bayes.R
@@ -8,6 +8,9 @@ data_matrix <- matrix(c(50, 30, 20, 45, 25, 30), nrow = 2, byrow = TRUE,
                                       Color = c("Red", "Blue", "Green")))
 
 # 베이즈 독립성 검정
+# sampleType = "indepMulti": 행이나 열의 합계가 고정된 경우 사용
+# fixedMargin = "rows": 남/여(행)의 인원수를 100명씩 고정했음을 명시
 independence_test <- contingencyTableBF(as.table(data_matrix),
-                                        sampleType = "jointMulti")
+                                        sampleType = "indepMulti",
+                                        fixedMargin = "rows")
 print(independence_test)
